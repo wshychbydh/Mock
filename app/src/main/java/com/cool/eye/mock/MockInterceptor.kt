56746519e088d6d9d0cb2447ae096a604cd2eab7
chuanbo <yangchuanbo@fcbox.com>
2021-12-13 23:12:04 +0800

    add default mock response

--- app/src/main/java/com/cool/eye/mock/MockInterceptor.kt
+++ app/src/main/java/com/cool/eye/mock/MockInterceptor.kt
@@ -2,9 +2,12 @@ package com.cool.eye.mock
 
 import android.os.SystemClock
 import okhttp3.Interceptor
+import okhttp3.Protocol
 import okhttp3.Response
 import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
+import java.io.IOException
+import java.lang.Exception
 
 /**
  * Created by ycb on 6/18/21
@@ -14,7 +17,22 @@ class MockInterceptor : Interceptor {
   override fun intercept(chain: Interceptor.Chain): Response {
 
     val request = chain.request()
-    val response = chain.proceed(request)
+
+    val response: Response = try {
+      chain.proceed(request)
+    } catch (e: IOException) {
+      if (MockHelper.mockAble) {
+        Response.Builder()
+            .request(request)
+            .protocol(Protocol.HTTP_1_1)
+            .code(200)
+            .message("Mock api")
+            .body("{}".toResponseBody())
+            .build()
+      } else {
+        throw e
+      }
+    }
 
     if (!MockHelper.mockAble) return response
 
